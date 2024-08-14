637e4b3fa177c79ccffbd4bfcc56b6a1caecb011
o0nekov0o <nydeanor@gmail.com>
2024-08-14 05:24:59 +0200

    Amélioration de l'architecture modulaire OK

--- lettings/models.py
+++ lettings/models.py
@@ -1,9 +1,8 @@
-"""from django.db import models
+from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
-from django.contrib.auth.models import User"""
 
 
-"""class Address(models.Model):
+class Address(models.Model):
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
@@ -21,11 +20,3 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username"""
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from oc_lettings_site import views as oc_lettings_site_views
+from lettings import views as lettings_views
+from profiles import views as profiles_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', oc_lettings_site_views.index, name='index'),
+    path('lettings/', lettings_views.index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', lettings_views.letting, name='letting'),
+    path('profiles/', profiles_views.index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles_views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
